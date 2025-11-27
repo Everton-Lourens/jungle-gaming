c675e1c70dc9bc29df465840644496fda99df0a3
everton-lourens <evertonlourens@gmail.com>
2025-11-27 11:38:29 -0300

    refactor: Corrigindo servidor express

--- src/shared/http/index.ts
+++ src/shared/http/index.ts
@@ -1,19 +1,21 @@
-import express, { type Express } from 'express'
-import { routes } from '../../routes/index.js';
+import bodyParser from 'body-parser';
+import express, { Router, type Express } from 'express'
 
 interface CustomExpress extends Express {
   postgres?: any
 }
 
 const PORT = process.env.NODE_ENV === 'production' ? (Number(process.env.PORT) || 8080) : 9999;
-const app: CustomExpress = express()
+const app: CustomExpress = express();
+const apiRouter = Router();
 
-app.use(routes);
+app.use(bodyParser.json())
+app.use('/api', apiRouter)
 
-app.get('/', async (req: any, res: any) => {
-  try {
-    res.status(200).send(`<h1>Servidor rodando na porta ${PORT}</h1>`)
-  } catch (err) {
-    res.status(500).send('<h1>Falha ao iniciar o servidor</h1>', err)
-  }
+apiRouter.get('/', (req, res, _) => {
+  res.status(200).send(`<h1>Servidor rodando na porta ${PORT}</h1>`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
