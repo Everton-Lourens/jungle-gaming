6f2ab4a4326650b8c9681205ce61373a464c908c
everton-lourens <evertonlourens@gmail.com>
2025-11-27 11:16:04 -0300

    build: Iniciando construção do servidor express

--- src/shared/http/index.ts
+++ src/shared/http/index.ts
@@ -1,8 +1,19 @@
-import express from 'express';
+import express, { type Express } from 'express'
+import { routes } from '../../routes/index.js';
 
-export const app = express();
-
-function helloWorld(name: string) {
-  console.log(name);
+interface CustomExpress extends Express {
+  postgres?: any
 }
-helloWorld("Everton Lourens!!");
+
+const PORT = process.env.NODE_ENV === 'production' ? (Number(process.env.PORT) || 8080) : 9999;
+const app: CustomExpress = express()
+
+app.use(routes);
+
+app.get('/', async (req: any, res: any) => {
+  try {
+    res.status(200).send(`<h1>Servidor rodando na porta ${PORT}</h1>`)
+  } catch (err) {
+    res.status(500).send('<h1>Falha ao iniciar o servidor</h1>', err)
+  }
+})
