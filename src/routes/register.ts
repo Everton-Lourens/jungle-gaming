9181973236146a25541a29855322476813aed3c1
everton-lourens <evertonlourens@gmail.com>
2025-11-27 11:43:21 -0300

    build criando e corrigindo rota de 'register

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -2,5 +2,8 @@ import express from 'express'
 
 const registerRoutes = express.Router()
 
+registerRoutes.get('/', (req, res, _) => {
+  res.status(200).send(`<h1>Servidor acessando o endpoint /register!</h1>`)
+})
 
 export { registerRoutes }
